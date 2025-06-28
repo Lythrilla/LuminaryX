83dc2aed67f20c307eb54b79b4dc8fc0b1551668
Lythrilla <xmk23333@gmail.com>
2025-06-28 12:46:43 +0800

    Set up project structure

---
+++ src/main/kotlin/org/Lythrilla/luminaryX/LuminaryX.kt
@@ -0,0 +1,14 @@
+package org.Lythrilla.luminaryX
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LuminaryX : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
